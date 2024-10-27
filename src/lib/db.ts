dce999d2d9c635f2e5c3c6959863b67659eaf237
vladholomah12 <vladholomah@ukr.net>
2024-10-27 23:44:53 +0200

    Update ExchangeDisplay and styles25

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,10 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['query', 'error', 'warn'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export { prisma }
